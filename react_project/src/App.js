fd59c80df5a71a9cd3484f3f29f4ea4044a965dd
Andreu Verdés <averdes12@gmail.com>
2024-12-02 13:53:26 +0100

    Addded Search Input

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import Callback from "./components/callback";
 import ElectronicArtists from "./components/electronicArtists";
+import SearchInput from "./components/searchInput";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/callback" element={<Callback />} />
         <Route path="/artists" element={<ElectronicArtists />} />
+        <Route path="/search" element= {<SearchInput />} />
       </Routes>
     </Router>
   );
