a678ff20f454d37a387501e5733530a0a5afc5d1
Andreu Verdés <averdes12@gmail.com>
2025-01-16 01:40:40 +0100

    mejorado rendimiento embeds no es para nada perfecto

--- react_project/src/components/list_components/playlistCard.js
+++ react_project/src/components/list_components/playlistCard.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useInView } from "react-intersection-observer";
 
 const PlaylistCard = ({ id }) => {
+
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.25,
+    });
+
     return (
-        <li>
-            <iframe
-            src={`https://open.spotify.com/embed/playlist/${id}`}
-            width="300"
-            height="80"
-            allow="encrypted-media"
-        ></iframe>
+        <li ref={ref}>
+            {inView && (
+                <iframe
+                    src={`https://open.spotify.com/embed/playlist/${id}`}
+                    width="300"
+                    height="80"
+                    allow="encrypted-media"
+                ></iframe>
+            )}
         </li> 
     );
 };
--- react_project/src/components/list_components/trackCard.js
+++ react_project/src/components/list_components/trackCard.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useInView } from "react-intersection-observer";
 
 const TrackCard = ({ id }) => {
+
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
+
     return (
-        <li>
-            <iframe
-            src={`https://open.spotify.com/embed/track/${id}`}
-            width="300"
-            height="80"
-            allow="encrypted-media"
-        ></iframe>
+        <li ref={ref}>
+            {inView && (
+                <iframe
+                    src={`https://open.spotify.com/embed/track/${id}`}
+                    width="300"
+                    height="80"
+                    allow="encrypted-media"
+                ></iframe>
+            )}
         </li> 
     );
 };
